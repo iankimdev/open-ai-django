164f85e5e20ccacb1e975903c106acffe4d5eb03
ian <iMac@ians-iMac.local>
2023-05-08 16:56:37 +0900

    Created Product Detail & Update View

--- django/products/views.py
+++ django/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from .models import Product
@@ -22,3 +22,18 @@ def product_create_view(request):
 def product_list_view(request):
     object_list = Product.objects.all()
     return render(request, 'products/list.html', {"object_list":object_list})
+
+def product_detail_view(request, handle=None):
+    obj = get_object_or_404(Product, handle=handle)
+    is_owner = False
+    if request.user.is_authenticated:
+        is_owner = obj.user == request.user
+    context = {"object": obj, "is_owner": is_owner}
+
+    if is_owner:
+        form = ProductForm(request.POST or None, instance = obj)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+        context['form'] = form
+    return render(request, 'products/detail.html', context)
