8f187b828a4288c029bc099ccbac34c66ca9abd6
ian <ian.kim.dev@gmail.com>
2023-06-01 12:56:50 +0900

    fix(dalle): Fix errors(no user input and OPEN AI's usage limit has been reached) and use DRF

--- django/dalle/urls.py
+++ django/dalle/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import download_image, generate_image_from_txt_for_custom
+from .views import download_image, generate_image
 
 app_name = 'dalle'
 
 urlpatterns = [
-    path('custom', generate_image_from_txt_for_custom, name='custom'),
+    path('custom/', generate_image, name='custom'),
     path("download/<int:image_id>/", download_image, name="download_image"),
 ]
--- django/dalle/views.py
+++ django/dalle/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-import openai, os, requests
+import openai, os, requests, openai.error
 from django.core.files.base import ContentFile
 from .models import DalleImage
 from django.contrib.auth.decorators import login_required
 from core.env import config
-from products.models import Product
+from rest_framework.decorators import api_view
+from openai import error as openai_error
+
+
+
 # Create your views here.
 api_key= config("OPENAI_KEY", default=None)
-#api_key = os.getenv("OPENAI_KEY")
 openai.api_key = api_key
 
 
@@ -21,26 +24,30 @@ def download_image(request, image_id):
         response["Content-Disposition"] = f"attachment; filename={image.ai_image.name}"
         return response
 
+@api_view(['GET', 'POST'])
 @login_required
-def generate_image_from_txt_for_custom(request):
-    obj = None
+def generate_image(request):
+    dalle = None
+    error_message = None
     if api_key is not None and request.method == 'POST':
         user_input = request.POST.get("user_input")
-        response = openai.Image.create(
-            prompt = user_input,
-            size = "512x512",
-        )
-        #print(response)
-        img_url = response["data"][0]["url"]
-        img_response = requests.get(img_url)
-        img_file = ContentFile(img_response.content) #Bytes => Images
-        #print(img_file)
-        count = DalleImage.objects.count() + 1
-        #fname = f"image-{count}.jpg"
-        fname = f"image-{count}.jpg"
-        
-        obj = DalleImage(phrase=user_input)
-        obj.ai_image.save(fname, img_file)
-        obj.save()
-    return render(request, "custom/custom-dalle.html", {"product":obj})
+        if user_input:
+            try:
+                response = openai.Image.create(
+                    prompt=user_input,
+                    size="512x512",
+                )
+                img_url = response["data"][0]["url"]
+                img_response = requests.get(img_url)
+                img_file = ContentFile(img_response.content)
+                count = DalleImage.objects.count() + 1
+                fname = f"image-{count}.jpg"
 
+                dalle = DalleImage(phrase=user_input)
+                dalle.ai_image.save(fname, img_file)
+                dalle.save()
+            except openai_error.InvalidRequestError as e:
+                if str(e) == "Billing hard limit has been reached":
+                    error_message = "OPEN AI's usage limit has been reached. Please contact support for assistance. OPEN AI의 사용량이 초과하였습니다. 저에게 알려주시면 처리하겠습니다."
+                    return render(request, "custom/custom-dalle.html", {"error_message": error_message})
+    return render(request, "custom/custom-dalle.html", {"product": dalle})
