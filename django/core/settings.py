9cc6b6b2e94404bbb6e4a1d1f685b4f0851f2633
ian <ian.kim.dev@gmail.com>
2023-06-06 13:57:07 +0900

    fix(storages): Fix AuthorizationQueryParametersError

--- django/core/settings.py
+++ django/core/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'core.urls'
--- django/core/storages/conf.py
+++ django/core/storages/conf.py
@@ -1,12 +1,11 @@
 from core.env import config
-from django.conf import settings
 
 AWS_ACCESS_KEY_ID=config("AWS_ACCESS_KEY_ID", default=None)
 AWS_SECRET_ACCESS_KEY=config("AWS_SECRET_ACCESS_KEY", default=None)
-AWS_S3_SIGNATURE_VERSION="s3v4"
+AWS_S3_ADDRESSING_STYLE = "virtual"
 
-AWS_STORAGE_BUCKET_NAME="ai-gallery"
-AWS_STORAGE_BUCKET_REGION="ap-northeast-2"
+AWS_STORAGE_BUCKET_NAME=config("AWS_STORAGE_BUCKET_NAME", default="ai-gallery")
+AWS_S3_REGION_NAME="ap-northeast-2"
 
 AWS_DEFAULT_ACL="public-read"
 AWS_S3_USE_SSL=True
